2328b13666e2e377d7af3f7c25017dfc6e343969
abdallah <abdallah.araafat@gmail.com>
2025-05-24 22:33:14 +0300

    numbers factorial

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+      if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+let args = process.argv.slice(2);
+let firstArg = args[0];
+let mynumber = parseInt(firstArg);
+factorialResult = factorial(mynumber);
+console.log(factorialResult);
