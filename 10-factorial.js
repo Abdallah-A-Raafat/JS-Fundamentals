28e648bc722e7c4ea02f1322ed6090c465de48c4
abdallah <abdallah.araafat@gmail.com>
2025-05-24 22:36:33 +0300

    fixing numbers factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -8,5 +8,9 @@ function factorial(n) {
 let args = process.argv.slice(2);
 let firstArg = args[0];
 let mynumber = parseInt(firstArg);
+if (isNaN(mynumber)) {
+    console.log(1);
+}
+
 factorialResult = factorial(mynumber);
 console.log(factorialResult);
